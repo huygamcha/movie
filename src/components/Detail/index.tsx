82ee837618d031b9e184fdce7921acfaa6e0fe56
huygamcha <lehuynhhuy2002@gmail.com>
2023-08-22 16:18:03 +0700

    up

--- src/components/Detail/index.tsx
+++ src/components/Detail/index.tsx
@@ -28,6 +28,19 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./Detail.module.css";
 import clsx from "clsx";
+import CircleMark from "../CircleMark";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import {
+  AiOutlineUnorderedList,
+  AiFillHeart,
+  AiTwotoneAppstore,
+  AiFillStar,
+} from "react-icons/ai";
+import Icon from "../Icon";
 interface Genres {
   id: number;
   name: string;
@@ -43,11 +56,22 @@ interface dataType {
   runtime: number;
   vote_average: number;
   overview: string;
+  poster_path: string;
+}
+interface Cast {
+  original_name: string;
+  profile_path: string;
+  character: string;
+  id: number;
+}
+interface Character {
+  id: number;
+  cast: Cast[];
 }
 
 const Detail = () => {
   const [data, setData] = useState<dataType>();
-  const [character, setCharacter] = useState<dataType>();
+  const [character, setCharacter] = useState<Character>();
   const { id } = useParams(); // Lấy tham số "id" từ đường dẫn
   useEffect(() => {
     axios
@@ -78,31 +102,106 @@ const Detail = () => {
         console.log(error);
       });
   }, [id]);
+
   return (
     <div className={clsx(styles.wrapper, "container", "mx-auto")}>
       {data && (
         <div className={clsx(styles.content)}>
-          <div>
+          <div className={clsx(styles.content_img)}>
             <img
               className={clsx(styles.img)}
-              src={`https://www.themoviedb.org/t/p/w220_and_h330_face${data.backdrop_path}`}
+              src={`https://www.themoviedb.org/t/p/w220_and_h330_face${data.poster_path}`}
               alt=""
             />
           </div>
 
-          <div>
-            {" "}
-            <h2>{data.original_title}</h2>
-            <br />
-            {data.release_date}
-            {data.genres.map((genre, i) => (
-              <span key={i}>{genre.name}</span>
-            ))}
-            {`${Math.floor(data.runtime / 60)}h${data.runtime % 60}m`}
+          <div className={clsx(styles.content_text)}>
+            <h2>
+              {data.original_title} ({data.release_date.slice(0, 4)})
+            </h2>
+            <ul className={clsx(styles.space)}>
+              <li>Ngày phát hành: {data.release_date} </li>
+              <li className={clsx(styles.ul)}>
+                Thể loại:
+                <ul className={clsx(styles.ul)}>
+                  {data.genres.map((genre, i) => (
+                    <li key={i}>
+                      {i < data.genres.length - 1
+                        ? `${genre.name},`
+                        : `${genre.name}`}
+                    </li>
+                  ))}
+                </ul>
+              </li>
+              <li>
+                <ul className={clsx(styles.ul, styles.action)}>
+                  <li className={clsx("flex", "items-center")}>
+                    <CircleMark degree={data.vote_average} />
+                    <span className={clsx("pl-1")}>User Score</span>
+                  </li>
+                  <li>
+                    <AiOutlineUnorderedList />{" "}
+                  </li>
+                  <li>
+                    <AiFillHeart />
+                  </li>
+                  <li>
+                    <AiTwotoneAppstore />
+                  </li>
+                  <li>
+                    <AiFillStar />
+                  </li>
+                  <li className={clsx("flex", "items-center")}>
+                    <Icon />
+                    <span className={clsx("pl-1")}> Play now</span>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                Thời lượng:
+                {` ${Math.floor(data.runtime / 60)}h ${data.runtime % 60}m`}
+              </li>
+              <li>
+                Tổng quan:
+                <br></br>
+                {data.overview}
+              </li>
+            </ul>
           </div>
         </div>
       )}
-      {character && <div>{character.id}</div>}
+      {character && (
+        <div>
+          <h4> Top Billed Cast:</h4>
+          <div>
+            <Swiper
+              modules={[Navigation, Pagination, Scrollbar]}
+              navigation
+              // scrollbar={{ draggable: true, dragSize: 100 }}
+              spaceBetween={15}
+              slidesPerView={7}
+            >
+              {character.cast.map((item) => (
+                <SwiperSlide key={item.id}>
+                  <div className={clsx(styles.character_info)}>
+                    <img
+                      className={clsx(styles.img_character)}
+                      src={`https://www.themoviedb.org/t/p/w220_and_h330_face${item.profile_path}`}
+                      alt={item.profile_path}
+                    />
+                    <div className={clsx(styles.character_actor)}>
+                      {item.character}
+                    </div>
+                    <div className={clsx(styles.character_name_origin)}>
+                      {item.original_name}
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
